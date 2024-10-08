5b0e919678693dc9a09a4561c11ec413e83064ee
Pedro Henrique Medeiros <pedro9473@hotmail.com>
2024-10-08 00:52:20 -0300

    feat: implementação do projeto.

---
+++ project23/src/main/java/Project23.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Project23 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Digite o número inicial: ");
+        int n1 = in.nextInt();
+        System.out.println("Digite o número secundário: ");
+        int n2 = in.nextInt();
+        int soma = 0;
+        int i = n1;
+        while(i <= n2){
+            soma = soma + i;
+            i++;
+        }
+        System.out.println("Soma dos valores: " + soma);
+    }
+}
